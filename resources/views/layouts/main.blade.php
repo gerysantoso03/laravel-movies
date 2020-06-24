2328e11bc220a00fe57a57c588ffdbc6b23e2c0c
gerysantoso03 <Gerysantos03@gmail.com>
2020-06-24 22:43:42 +0700

    Update input search style

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -29,7 +29,7 @@
             </ul>
 
             <div class="flex flex-col md:flex-row items-center">
-                <div class="relative mt-3 md:mt-8 mt-3 md:mt-8">
+                <div class="relative mt-3 md:mt-0">
                     <input type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1
                     focus:outline-none focus:shadow-outline" placeholder="Search">
                     <div class="absolute top-0">
