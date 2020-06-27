24fb2e648f3ce23c8c88cdd2d0fe962634eae011
gerysantoso03 <Gerysantos03@gmail.com>
2020-06-27 14:54:48 +0700

    Added livewire search dropdown and tailwind css spinner

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,8 @@
 
     {{-- Tailwind CSS --}}
     <link rel="stylesheet" href="/css/main.css">
+    {{-- Livewire Styles --}}
+    <livewire:styles>
 </head>
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800">
@@ -29,14 +31,7 @@
             </ul>
 
             <div class="flex flex-col md:flex-row items-center">
-                <div class="relative mt-3 md:mt-0">
-                    <input type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1
-                    focus:outline-none focus:shadow-outline" placeholder="Search">
-                    <div class="absolute top-0">
-                        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
-                    </div>
-                </div>
-
+                <livewire:search-dropdown/>
                 <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="#">
                         <img src="/img/GerySantoso.jpg" alt="GerySantoso" class="rounded-full w-10 h-10">
@@ -46,5 +41,7 @@
         </div>
     </nav>
     @yield('content')
+    {{-- Livewire Script --}}
+    <livewire:scripts>
 </body>
 </html>
