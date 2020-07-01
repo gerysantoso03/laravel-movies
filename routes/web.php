6aabde56ddeba5686d2a16e0c6d053e788ddd145
gerysantoso03 <Gerysantos03@gmail.com>
2020-07-01 20:14:11 +0700

    Adding route for actors

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Movies controller
 Route::get('/', 'moviesController@index')->name('movies.index');
 Route::get('/movies/{movie}', 'moviesController@show')->name('movies.show');
+
+// Actors controller
+Route::get('/actors', 'actorsController@index')->name('actors.index');
+Route::get('/actors/page/{page?}', 'actorsController@index');
+
+Route::get('/actors/{actor}', 'actorsController@show')->name('actors.show');
