2dbaac6c5312d26c7ac6d1cef1e8ff1bdab402f5
gerysantoso03 <Gerysantos03@gmail.com>
2020-07-02 16:52:14 +0700

    Adding tvshow route

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 // Movies controller
 Route::get('/', 'moviesController@index')->name('movies.index');
-Route::get('/movies/{movie}', 'moviesController@show')->name('movies.show');
+Route::get('/movies/{id}', 'moviesController@show')->name('movies.show');
 
 // Actors controller
 Route::get('/actors', 'actorsController@index')->name('actors.index');
 Route::get('/actors/page/{page?}', 'actorsController@index');
 
-Route::get('/actors/{actor}', 'actorsController@show')->name('actors.show');
+Route::get('/actors/{id}', 'actorsController@show')->name('actors.show');
+
+// Tv show controller 
+Route::get('/tvshow', 'tvshowController@index')->name('tvshow.index');
+Route::get('/tvshow/{id}', 'tvshowController@show')->name('tvshow.show');
